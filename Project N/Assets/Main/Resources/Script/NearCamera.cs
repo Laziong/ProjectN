d7457ff4c5cc35fa2347f223fc11bd310dd9b7fd
Laziong <s21220067@nucba.ac.jp>
2016-08-06 02:54:46 +0900

    2016/8/6

--- Project N/Assets/Main/Resources/Script/NearCamera.cs
+++ Project N/Assets/Main/Resources/Script/NearCamera.cs
@@ -3,11 +3,6 @@ using System.Collections;
 
 public class NearCamera : MonoBehaviour {
 
-	public GameObject Sesystem;
-
-	public GameObject targetobj;
-
-
 	// Use this for initialization
 	void Start () {
 	}
@@ -20,19 +15,5 @@ public class NearCamera : MonoBehaviour {
 		}else{
 			this.GetComponent<Camera>().enabled = true;
 		}
-		/*
-		targetobj = Sesystem.gameObject.GetComponent<SelectSystem>().hitobj;
-		Vector3 pos = targetobj.transform.position;
-
-
-		if (TurnManager.once == true) {
-			this.transform.position = new Vector3 (pos.x, pos.y + 0.7f, pos.z - 2);
-			Quaternion roteC = Quaternion.Euler (0.0f, 0.0f, 0.0f);
-		} else if
-			(TurnManager.once == false) {
-			this.transform.position = new Vector3 (pos.x, pos.y + 0.7f, pos.z + 2);
-			this.transform.rotation = Quaternion.Euler(0.0f,180.0f,0.0f);
-		}
-		*/
 	}
 }
